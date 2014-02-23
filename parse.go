1d30a748eaa9f22f476e92f384f6cf140160e049
Jeff Buchbinder <jeffreymbuchbinder@gmail.com>
2014-02-23 14:34:23 -0500

    Properly support layered structures.

--- parse.go
+++ parse.go
@@ -1,6 +1,7 @@
 package gofixedfield
 
 import (
+	"fmt"
 	"io/ioutil"
 	"reflect"
 	"strconv"
@@ -30,16 +31,29 @@ func RecordsFromFile(filename string, eolstyle string) ([]string, error) {
 // resemble:
 //
 // 	type SomeType struct {
-// 		ValA string `fixed:"1-5"`
-//		ValB int    `fixed:"10-15"`
+// 		ValA string        `fixed:"1-5"`
+//		ValB int           `fixed:"10-15"`
+//		ValC *EmbeddedType `fixed:"16-22"`
 // 	}
+//	type EmbeddedType struct {
+//		ValX string `fixed:"1-3"`
+//		ValY string `fixed:"4-6"`
+//	}
 //
 //	var out SomeType
 //	err := Unmarshal("some string here", &out)
 //
 // String offsets are one based, not zero based.
 func Unmarshal(data string, v interface{}) error {
-	val := reflect.ValueOf(v).Elem()
+	//debugStruct(v)
+	var val reflect.Value
+	if reflect.TypeOf(v).Name() != "" {
+		val = reflect.ValueOf(v)
+	} else {
+		val = reflect.ValueOf(v).Elem()
+	}
+
+	//fmt.Printf("Found %d fields\n", val.NumField())
 	for i := 0; i < val.NumField(); i++ {
 		typeField := val.Type().Field(i)
 		tag := typeField.Tag
@@ -48,6 +62,7 @@ func Unmarshal(data string, v interface{}) error {
 		cBookend := strings.Split(cRange, "-")
 		if len(cBookend) != 2 {
 			// If we don't have two values, skip
+			//fmt.Println("Two tag values not found")
 			continue
 		}
 
@@ -58,38 +73,66 @@ func Unmarshal(data string, v interface{}) error {
 		//e -= 1
 
 		// Sanity check range before dying miserably
-		if b < 0 || e >= len(data) {
+		if b < 0 || e > len(data) {
+			//fmt.Printf("Failed sanity check for b = %d, e = %d, len(data) = %d\n", b, e, len(data))
 			continue
 		}
 
 		s := data[b:e]
 
+		//fmt.Printf("Field found of type %s\n", typeField.Type.Kind())
+
 		switch typeField.Type.Kind() {
 		case reflect.String:
+			//fmt.Printf("Found string value '%s'\n", s)
 			val.Field(i).SetString(s)
 			break
 		case reflect.Int:
+			//fmt.Printf("Found value '%s'\n", s)
 			v, err := strconv.ParseInt(s, 10, 64)
 			if err != nil {
+				fmt.Println(err.Error())
 				continue
 			}
 			val.Field(i).SetInt(v)
 			break
 		case reflect.Uint:
+			//fmt.Printf("Found uint value '%s'\n", s)
 			v, err := strconv.ParseUint(s, 10, 64)
 			if err != nil {
+				fmt.Println(err.Error())
 				continue
 			}
 			val.Field(i).SetUint(v)
 			break
-		case reflect.Struct:
+		case reflect.Ptr, reflect.Struct:
+			//fmt.Printf("Found ptr/str value '%s'\n", s)
+
 			// Handle embedded objects by recursively parsing
 			// the object with the range we passed.
-			Unmarshal(s, val.Field(i).Interface())
+			if val.Field(i).IsNil() {
+				// Initialize pointer to avoid panic
+				val.Field(i).Set(reflect.New(val.Field(i).Type().Elem()))
+			}
+			err := Unmarshal(s, val.Field(i).Interface())
+			if err != nil {
+				fmt.Println(err.Error())
+			}
 			break
 		default:
+			//fmt.Println("Found unknown value '%s'", s)
 			break
 		}
 	}
 	return nil
+}
+
+func debugStruct(i interface{}) {
+	val := reflect.ValueOf(i).Elem()
+	for i := 0; i < val.NumField(); i++ {
+		valueField := val.Field(i)
+		typeField := val.Type().Field(i)
+		tag := typeField.Tag
+		fmt.Printf("Field Name: %s, Field Value: %v, Tag Value: %s, Type: %s\n", typeField.Name, valueField.Interface(), tag.Get("fixed"), typeField.Type.Kind())
+	}
 }
--- parse_test.go
+++ parse_test.go
@@ -5,7 +5,8 @@ import (
 )
 
 const (
-	BASIC_PARSE_TEST = "1234567890ABCDEFGHIJ"
+	BASIC_PARSE_TEST   = "1234567890ABCDEFGHIJ"
+	LAYERED_PARSE_TEST = "20091010EX"
 )
 
 type basicParseTest struct {
@@ -13,6 +14,17 @@ type basicParseTest struct {
 	NumberB int    `fixed:"3-5"` // test overlap
 	StringC string `fixed:"11-15"`
 	StringD string `fixed:"30-35"` // should fail
+}
+
+type layeredParseTest struct {
+	DateField   *dateStruct `fixed:"1-8"`
+	StringAfter string      `fixed:"9-10"`
+}
+
+type dateStruct struct {
+	Y int `fixed:"1-4"`
+	M int `fixed:"5-6"`
+	D int `fixed:"7-8"`
 }
 
 func TestBasicParsing(t *testing.T) {
@@ -30,5 +42,23 @@ func TestBasicParsing(t *testing.T) {
 	}
 	if out.StringD != "" {
 		t.Errorf("StringD should have failed to parse")
+	}
+}
+
+func TestLayeredParsing(t *testing.T) {
+	t.Log("Layered parsing test")
+	var out layeredParseTest
+	Unmarshal(LAYERED_PARSE_TEST, &out)
+	if out.StringAfter != "EX" {
+		t.Errorf("Failed to parse after embedded struct/ptr\n")
+	}
+	if out.DateField.Y != 2009 {
+		t.Errorf("Failed to parse embedded Y (Y=%d)\n", out.DateField.Y)
+	}
+	if out.DateField.M != 10 {
+		t.Errorf("Failed to parse embedded M (M=%d)\n", out.DateField.M)
+	}
+	if out.DateField.D != 10 {
+		t.Errorf("Failed to parse embedded D (D=%d)\n", out.DateField.D)
 	}
 }
